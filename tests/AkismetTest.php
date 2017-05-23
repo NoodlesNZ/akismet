9489145bebbc3ddba4378dcb7694583d5a509de1
Nick Le Mouton <nick@noodles.net.nz>
2017-05-24 11:59:41 +1200

    Add test case

---
+++ tests/AkismetTest.php
@@ -0,0 +1,75 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class AkismetTest extends TestCase
+{
+  private $akismet;
+  private $requestFactory;
+  
+  protected function setUp()
+  {
+    $this->requestFactory = new MockRequestFactory();
+    
+    $this->akismet = new Akismet('', '');
+    $this->akismet->setRequestFactory($this->requestFactory);
+  }
+  
+  public function testIsKeyValid_validKey()
+  {
+    $this->requestFactory->setResponse("\r\n\r\nvalid");
+    $response = $this->akismet->isKeyValid();
+    $this->assertTrue($response);
+  }
+  
+  public function testIsKeyValid_invalidKey()
+  {
+    $this->requestFactory->setResponse("\r\n\r\ninvalid");
+    $response = $this->akismet->isKeyValid();
+    $this->assertFalse($response);
+  }
+  
+  public function testIsCommentSpam()
+  {
+    $this->requestFactory->setResponse("\r\n\r\ntrue");
+    $response = $this->akismet->isCommentSpam();
+    $this->assertTrue($response);
+  }
+  
+  public function testIsCommentHam()
+  {
+    $this->requestFactory->setResponse("\r\n\r\nfalse");
+    $response = $this->akismet->isCommentSpam();
+    $this->assertFalse($response);
+  }
+}
+
+class MockRequestSender implements AkismetRequestSender
+{
+  private $response;
+  
+  
+  public function __construct($response)
+  {
+    $this->response = $response;
+  }
+  
+  public function send($host, $port, $request, $responseLength = 1160)
+  {
+    return $this->response;
+  }
+}
+
+class MockRequestFactory implements AkismetRequestFactory
+{
+  private $response;
+  
+  public function createRequestSender()
+  {
+    return new MockRequestSender($this->response);
+  }
+  
+  public function setResponse($response)
+  {
+    $this->response = $response;
+  }
+}
